56c9a728eda7955631278effb56f193bfe44a472
Evguen Stukun <enhotkh@gmail.com>
2026-02-20 10:03:02 +0200

    create page home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LogInOutComponent } from './pages/log-in-out/log-in-out.component';
+import { HomeComponent } from './pages/home/home/home.component';
 
 export const routes: Routes = [
-    {path:'', component:LogInOutComponent ,pathMatch:'full'}
+    {path:'', component:LogInOutComponent ,pathMatch:'full'},
+    {path:'home', component:HomeComponent ,pathMatch:'full'}
 ];
